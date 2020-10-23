8cac601f323bd8409d5bad8cf4b7b4ed1e061e72
João Rodrigues <rodrigues.jmatheus@gmail.com>
2020-10-23 19:23:03 -0300

    Inicia endpoint result

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, request, redirect, render_template, url_for
 from waitress import serve
 
 app = Flask(__name__)
@@ -6,15 +6,18 @@ app = Flask(__name__)
 city_options = [("15", "Samambaia"), ("16", "Recanto das Emas")]
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home_page():
+    city = request.form.get("city_select")
+    if city == "15" or city == "16":
+        return redirect(url_for('result', city_id=city))
+
     return render_template("home_page.html", data=city_options)
 
 
-@app.route("/result", methods=["GET", "POST"])
-def result():
-    select = request.form.get("city_select")
-    return(str(select))
+@app.route("/result/<city_id>", methods=["GET", "POST"])
+def result(city_id):
+    return(str(city_id))
 
 
 if __name__ == "__main__":

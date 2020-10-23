0853da694e959aee62e97ac018c4dd54fe7d175f
João Rodrigues <rodrigues.jmatheus@gmail.com>
2020-10-23 03:53:32 -0300

    Popula bus_routes_id

--- create_populate_database.py
+++ create_populate_database.py
@@ -1,4 +1,44 @@
 import os
+import requests
+
+
+origin_cities = [("15", "Samambaia"), ("16", "Recanto das Emas")]
+
+destiny_city = ("25", "Plano Piloto")
+
+base_url_bus_routes_id = ("https://www.sistemas.dftrans.df.gov.br/linha/ra/")
+
+bus_routes_id = {}
+
+
+def populate_bus_routes_id():
+    for origin_city in origin_cities:
+        response = requests.get(
+            base_url_bus_routes_id + origin_city[0] + "/ra/" + destiny_city[0]
+        ).json()
+
+        data = ""
+        path = origin_city[0] + "_" + destiny_city[0]
+
+        i = 0
+        for bus_route in response:
+            if bus_route['numero']:
+                bus_route_id = bus_route['numero']
+
+                if i == 0:
+                    i = 1
+                    data = bus_route_id
+                    bus_route_id[path] = [bus_route_id]
+                else:
+                    data = data + "," + bus_route_id
+                    bus_route_id[path].append(bus_route_id)
+
+        data_archive = open("./database/bus_routes_id/" + path + ".txt", 'w')
+        data_archive.writelines(data)
+
+
+def popula_database():
+    populate_bus_routes_id()
 
 
 def create_database():
@@ -8,6 +48,7 @@ def create_database():
 
 def main():
     create_database()
+    popula_database()
 
 
 if __name__ == '__main__':

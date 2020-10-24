a9399ae7adec90dcc922e32fd6cabfe1a87fa67b
João Rodrigues <rodrigues.jmatheus@gmail.com>
2020-10-23 21:49:27 -0300

    Leitura de dados do banco

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 city_options = [("15", "Samambaia"), ("16", "Recanto das Emas")]
 
+average_duration_bus_routes = {}
+
+bus_routes_start_times = []
+
 
 @app.route('/', methods=["GET", "POST"])
 def home_page():
@@ -17,7 +21,36 @@ def home_page():
 
 @app.route("/result/<city_id>", methods=["GET", "POST"])
 def result(city_id):
-    return(str(city_id))
+    data_bus_routes_id = open(
+        "./database/bus_routes_id/" + city_id + "_25.txt", 'r'
+    )
+    bus_routes_id = data_bus_routes_id.readlines()[0].split(",")
+    data_bus_routes_id.close()
+
+    for bus_route_id in bus_routes_id:
+        data_schedules_bus_route = open(
+            "./database/schedules_bus_routes/" + city_id + "_25/" +
+            bus_route_id + ".txt"
+        )
+        schedules_bus_route = (
+            data_schedules_bus_route.readlines()[0].split(',')
+        )
+        data_schedules_bus_route.close()
+
+        average_duration_bus_routes[bus_route_id] = 2*int(
+            schedules_bus_route[0]
+        )
+
+        for schedule_bus_route in schedules_bus_route[1:]:
+            schedule = schedule_bus_route.split(':')
+            hours = int(schedule[0])
+            minutes = int(schedule[1])
+
+            start_time = hours*60 + minutes
+            bus_routes_start_times.append((start_time, bus_route_id))
+
+    bus_routes_start_times.sort()
+    return(str(bus_routes_start_times))
 
 
 if __name__ == "__main__":

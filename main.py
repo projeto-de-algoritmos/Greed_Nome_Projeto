714b573e2d1e6deb7060f406c0b9a77e3232c181
João Rodrigues <rodrigues.jmatheus@gmail.com>
2020-10-23 23:11:45 -0300

    Implementa result

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 
 city_options = [("15", "Samambaia"), ("16", "Recanto das Emas")]
 
-average_duration_bus_routes = {}
-
-bus_routes_start_times = []
-
 
 @app.route('/', methods=["GET", "POST"])
 def home_page():
@@ -21,6 +17,9 @@ def home_page():
 
 @app.route("/result/<city_id>", methods=["GET", "POST"])
 def result(city_id):
+    average_duration_bus_routes = {}
+    bus_routes_start_times = []
+
     data_bus_routes_id = open(
         "./database/bus_routes_id/" + city_id + "_25.txt", 'r'
     )
@@ -50,7 +49,49 @@ def result(city_id):
             bus_routes_start_times.append((start_time, bus_route_id))
 
     bus_routes_start_times.sort()
-    return(str(bus_routes_start_times))
+
+    number_buses = 0
+    bus_occupancy_end_time = []
+    bus_data = {}
+    for bus_routes_start_time in bus_routes_start_times:
+        average_duration_bus_route = (
+            average_duration_bus_routes[bus_routes_start_time[1]]
+        )
+        bus_routes_end_time = (
+            bus_routes_start_time[0] + average_duration_bus_route
+        )
+
+        if bus_occupancy_end_time and (
+            bus_routes_start_time[0] > bus_occupancy_end_time[0][0]
+        ):
+            bus = int(bus_occupancy_end_time.pop(0)[1])
+            bus_occupancy_end_time.append((bus_routes_end_time, str(bus)))
+            bus_occupancy_end_time.sort()
+
+            bus_data[bus].append(
+                (
+                    bus_routes_start_time[0],
+                    bus_routes_start_time[1],
+                    bus_routes_end_time
+                )
+            )
+        else:
+            number_buses = number_buses + 1
+            bus_occupancy_end_time.append(
+                (bus_routes_end_time, str(number_buses))
+            )
+            bus_occupancy_end_time.sort()
+
+            bus_data[number_buses] = []
+            bus_data[number_buses].append(
+                (
+                    bus_routes_start_time[0],
+                    bus_routes_start_time[1],
+                    bus_routes_end_time
+                )
+            )
+
+    return render_template("result.html", data=number_buses)
 
 
 if __name__ == "__main__":
